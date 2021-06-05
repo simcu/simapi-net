1c8ea86fd31bb01b84b2fd50f86bbe57a9e3aedf
xRain <xrain@simcu.com>
2021-06-05 11:20:32 +0800

    add basemodel

---
+++ Models/SimApiBaseModel.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Text.Json;
+
+namespace SimApi.Models
+{
+    public class SimApiBaseModel
+    {
+        protected virtual string[] MapperIgnoreField { get; set; } = {"Id", "CreatedAt", "UpdatedAt"};
+        protected virtual string UpdatedTimeField { get; set; } = "UpdatedAt";
+
+        public void MapData<TS>(TS source, bool mapAll = false)
+        {
+            //获取要赋值的源数据不为null的项目
+            var sourceProps = source.GetType().GetProperties().Where(x => x.GetValue(source) != null)
+                .Select(x => new {x.Name, x.PropertyType})
+                .ToDictionary(x => x.Name, x => x.PropertyType);
+
+            //获取目标对象的属性信息
+            var targetProps = GetType().GetProperties().Select(x => new {x.Name, x.PropertyType})
+                .ToDictionary(x => x.Name, x => x.PropertyType);
+            foreach (var sp in sourceProps)
+            {
+                //检查源对象不为空的属性是否在目标对象中存在
+                if (targetProps.ContainsKey(sp.Key) && sp.Value == targetProps[sp.Key] &&
+                    (!MapperIgnoreField.Contains(sp.Key) || mapAll))
+                {
+                    GetType().GetProperty(sp.Key)?
+                        .SetValue(this, source.GetType().GetProperty(sp.Key)?.GetValue(source));
+                }
+            }
+
+            UpdateTime();
+        }
+
+        /// <summary>
+        /// 更新update时间
+        /// </summary>
+        /// <returns></returns>
+        public void UpdateTime()
+        {
+            GetType().GetProperty(UpdatedTimeField)?.SetValue(this, DateTime.Now);
+        }
+    }
+}
