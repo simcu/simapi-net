6023cf2eb06c17f00bb1e9b37a6d8dbef3cf074f
xRain <xrain@simcu.com>
2022-06-29 03:03:51 +0800

    fix default option, add option description

--- Configurations/SimApiStorageOptions.cs
+++ Configurations/SimApiStorageOptions.cs
@@ -2,10 +2,23 @@ namespace SimApi.Configs
 {
     public class SimApiStorageOptions
     {
+        /// <summary>
+        /// S3 服务器入口地址
+        /// </summary>
         public string Endpoint { get; set; }
+
+        /// <summary>
+        /// S3 服务器文件访问地址
+        /// </summary>
         public string ServeUrl { get; set; }
+
+        /// <summary>
+        /// S3服务 Bucket
+        /// </summary>
         public string Bucket { get; set; }
+
         public string AccessKey { get; set; }
+
         public string SecretKey { get; set; }
     }
 }
